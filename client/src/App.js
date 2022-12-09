833b293fce99f3521480134053542ee7c9c0fcaa
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-12-08 21:18:58 -0500

    removed useeffect hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {ApolloClient, ApolloProvider, InMemoryCache,createHttpLink} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider  client ={client}>
     <Router>
       <>
         <Navbar />
-        <Routes>
+        <Switch>
           <Route 
             path='/' 
             element={<SearchBooks />} 
@@ -22,9 +47,10 @@ function App() {
             path='*'
             element={<h1 className='display-2'>Wrong page!</h1>}
           />
-        </Routes>
+        </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
